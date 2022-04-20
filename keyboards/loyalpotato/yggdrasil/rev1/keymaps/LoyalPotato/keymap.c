c9400ff3e41c845c476a9aa0edc0a5bcf0787593
LoyalPotato <david.bazilio.r@gmail.com>
2022-04-20 22:08:02 +0100

    Some formatting

--- keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
+++ keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
@@ -1,11 +1,12 @@
 #include QMK_KEYBOARD_H
+#include "rev1.h"
 
 enum yggdrasil_keymap_layers {
  BASE_LAYER,
  NUM_LAYER,
  SYM_LAYER,
  FN_LAYER,
- MEDIA_LAYER,
+ MEDIA_LAYER
 };
 
 #define KC_NUM_LAYER LT(NUM_LAYER, KC_QUOTE)
@@ -15,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SEMICOLON,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_NUM_LAYER,
-                          TT(SYM_LAYER), KC_LCTL, KC_LSFT,           KC_ENT,  KC_SPC,  KC_BSPC
+                          TT(SYM_LAYER), KC_LCTL, KC_LSFT,              KC_ENT,  KC_SPC,  KC_BSPC
     ),
     [SYM_LAYER] = LAYOUT_split_3x5_3(
         KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_LEFT_BRACKET,                               KC_RIGHT_BRACKET,    KC_PERC,    KC_AMPR,    KC_PEQL,    KC_PLUS,
@@ -40,5 +41,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                               XXXXXXX,    KC_AUDIO_VOL_UP,    KC_AUDIO_VOL_DOWN,    KC_AUDIO_MUTE,    XXXXXXX,
         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,                               XXXXXXX,    XXXXXXX,            XXXXXXX,              XXXXXXX,          XXXXXXX, 
                           TO(BASE_LAYER), KC_LCTL, KC_LSFT,                           KC_ENT,  KC_SPC,  KC_BSPC
-    )
+    ),
 };
+    
