deccd632ffc5c76d303f55e0df94167d644a300e
LoyalPotato <david.bazilio.r@gmail.com>
2022-04-20 08:59:07 +0100

    SECOND_LAYER in progress - NUMS & FN

--- keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
+++ keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
@@ -5,7 +5,7 @@
 
 enum yggdrasil_keymap_layers = {
  BASE_LAYER = 0,
- NUM_LAYER,
+ SECOND_LAYER,
  SYM_LAYER,
  MEDIA_LAYER,
  NAV_LAYER,
@@ -23,6 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           TT(SYM_LAYER), KC_LCTL, KC_LSFT,           KC_ENT,  KC_SPC,  KC_BSPC
     ),
     
+    [SECOND_LAYER] = LAYOUT_split_3x5_3(
+        KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SEMICOLON,
+        KC_F11,    KC_F12,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_NUM_LAYER,
+                          TT(SYM_LAYER), KC_LCTL, KC_LSFT,           KC_ENT,  KC_SPC,  KC_BSPC
+    ),
+
     [SYM_LAYER] = LAYOUT_split_3x5_3(
         KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_LEFT_BRACKET,                               KC_RIGHT_BRACKET,    KC_PERC,    KC_AMPR,    KC_PEQL,    KC_PLUS,
         KC_ASTR,    KC_PIPE,    KC_UNDS,    KC_F,    KC_LPRN,                                          KC_RPRN,    KC_RALT,    KC_RSFT,   KC_RCTL,    KC_PMNS,
