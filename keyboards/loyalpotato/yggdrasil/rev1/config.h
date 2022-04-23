326352b7bef7a71dac810efa0d5a51826ad30f5a
LoyalPotato <david.bazilio.r@gmail.com>
2022-04-23 15:36:46 +0100

    Fix reverse mapping on left side No comms still :(

--- keyboards/loyalpotato/yggdrasil/rev1/config.h
+++ keyboards/loyalpotato/yggdrasil/rev1/config.h
@@ -12,29 +12,15 @@
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 5
 
-#define MATRIX_ROW_PINS \
-  { D7, E6, B4, B5 }
-#define MATRIX_COL_PINS \
-  { F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { D7, E6, B4, B5 }
+//#define MATRIX_COL_PINS { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { B6, B2, B3, B1, F7 }
 
-#define MATRIX_ROW_PINS_RIGHT \
-  { D7, E6, B4, B5 }
-#define MATRIX_COL_PINS_RIGHT \
-  { F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS_RIGHT { F7, B1, B3, B2, B6 }
+
+#define MASTER_LEFT
+
+#define SOFT_SERIAL_PIN D2
+
 
 #define TAPPING_TOGGLE 2
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
