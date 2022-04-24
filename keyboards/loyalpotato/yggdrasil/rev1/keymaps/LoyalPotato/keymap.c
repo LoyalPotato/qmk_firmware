941e0933d0cde8db9dcd4cd3601b5dd3a8b9f90f
LoyalPotato <david.bazilio.r@gmail.com>
2022-04-24 17:59:33 +0100

    Fix indenting

--- keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
+++ keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
@@ -31,8 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUM_NAV_LAYER] = LAYOUT(
         KC_HOME,    KC_PAGE_UP,    KC_PAGE_DOWN,    KC_PAGE_DOWN,    KC_TRNS,                               KC_TRNS, KC_1,    KC_2,    KC_3,    KC_TRNS,
         KC_TRNS,    KC_LEFT,    KC_UP,    KC_RIGHT,    KC_TRNS,                                             KC_TRNS,    KC_4,    KC_5,    KC_6,    KC_TRNS,
-        KC_TRNS,    KC_TRNS,    KC_DOWN,    KC_TRNS,    KC_TRN
-        S,                                            KC_TRNS,     KC_7,    KC_8, KC_9,  XXXXXXX,
+        KC_TRNS,    KC_TRNS,    KC_DOWN,    KC_TRNS,    KC_TRNS,                                            KC_TRNS,     KC_7,    KC_8, KC_9,  XXXXXXX,
                                 TO(BASE_LAYER), KC_LCTL, KC_LSFT,                          MO(FN_LAYER),  KC_SPC,  KC_BSPC
     ),
     [FN_LAYER] = LAYOUT(
