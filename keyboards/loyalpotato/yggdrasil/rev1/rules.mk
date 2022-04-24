da0fc3e8593c55b5079eb571cac88cff6fd28e59
LoyalPotato <david.bazilio.r@gmail.com>
2022-04-24 17:52:36 +0100

    Fix typo :/ and add buunch of flags

--- keyboards/loyalpotato/yggdrasil/rev1/rules.mk
+++ keyboards/loyalpotato/yggdrasil/rev1/rules.mk
@@ -4,14 +4,19 @@ MCU = atmega32u4
 # Bootloader selection
 BOOTLOADER = caterina
 
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-
+RGB_MATRIX_ENABLE = no
 AUDIO_SUPPORTED = no        # Audio is not supported
 RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
 RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
-RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
 
-SPIT_KEYBOARD = yes
+SPLIT_KEYBOARD = yes
 LAYOUTS = split_3x5_3
