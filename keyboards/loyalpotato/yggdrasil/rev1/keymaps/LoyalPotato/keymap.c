4d4493d1b0dae0a7e326b01033dcf311ba64369f
LoyalPotato <david.bazilio.r@gmail.com>
2022-09-25 10:26:27 +0000

    Keymap work

--- keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
+++ keyboards/loyalpotato/yggdrasil/rev1/keymaps/LoyalPotato/keymap.c
@@ -3,7 +3,6 @@
 
 enum yggdrasil_keymap_layers {
  BASE_LAYER,
- MAC_BASE_LAYER,
  NUM_NAV_LAYER,
  SYM_LAYER,
  FN_LAYER,
@@ -12,34 +11,30 @@ enum yggdrasil_keymap_layers {
  GAME_LAYER,
 };
 
-#define KC_NUM_NAV_LAYER LT(NUM_NAV_LAYER, KC_QUOTE)
-#define KC_SYM_LAYER LT(SYM_LAYER, KC_ENT)
+#define SPC_NUM_NAV LT(NUM_NAV_LAYER, KC_SPC)
+#define ENT_SYM LT(SYM_LAYER, KC_ENT)
+
+#define LALT_LSHIFT MT(MOD_LSFT, KC_LALT) // Maybe make this tab?
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LAYER] = LAYOUT(
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SEMICOLON,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_NUM_NAV_LAYER,
-                          KC_LALT, KC_LCTL, KC_LSFT,                  KC_SYM_LAYER,  KC_SPC,  KC_BSPC
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_QUOTE,
+                          KC_LCTL, KC_LGUI, KC_LALT,                  ENT_SYM,  SPC_NUM_NAV,  KC_BSPC
     ),
     [SYM_LAYER] = LAYOUT(
         KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_LEFT_BRACKET,                                   KC_RIGHT_BRACKET,    KC_PERC,    KC_AMPR,    KC_PEQL,    KC_PLUS,
-        KC_ASTR,    KC_PIPE,    KC_UNDS,    TO(MAC_BASE_LAYER),    KC_LPRN,                                KC_RPRN,    KC_LALT,    KC_RSFT,   KC_DEL,    KC_PMNS,
-        KC_GRV,    KC_TILD,    KC_CIRC,    KC_PRINT_SCREEN,    TT(GAME_LAYER),                             KC_PSLS,    KC_QUES,    KC_BSLS,  KC_ESC,  TT(MEDIA_LAYER),
-                                      KC_LGUI, KC_LCTL, KC_TAB,                           XXXXXXX,   XXXXXXX,    XXXXXXX                 
-    ),
-    [MAC_BASE_LAYER] = LAYOUT(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SEMICOLON,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_NUM_NAV_LAYER,
-                          KC_LALT, KC_LGUI, KC_LSFT,                  KC_SYM_LAYER,  KC_SPC,  KC_BSPC
+        KC_ASTR,    KC_PIPE,    KC_UNDS,    KC_DEL,    KC_LPRN,                                        KC_RPRN,    KC_LALT,    KC_RSFT,   KC_LGUI,    KC_PMNS,
+        KC_GRV,    KC_TILD,    KC_CIRC,    KC_PRINT_SCREEN,    TT(GAME_LAYER),                          KC_PSLS,    KC_QUES,    KC_BSLS,  KC_ESC,  KC_LCTL,
+                                      KC_SPC, TT(MEDIA_LAYER), KC_TAB,                           XXXXXXX,   XXXXXXX,    XXXXXXX                 
     ),
     [NUM_NAV_LAYER] = LAYOUT(
         KC_HOME,    KC_PAGE_UP,    KC_PAGE_DOWN,    KC_PAGE_DOWN,    KC_END,                               KC_0, KC_1,    KC_2,    KC_3,    KC_TRNS,
-        KC_TRNS,    KC_LEFT,    KC_UP,    KC_RIGHT,    KC_TRNS,                                             KC_TRNS,    KC_4,    KC_5,    KC_6,    KC_TRNS,
-        KC_TRNS,    KC_TRNS,    KC_DOWN,    KC_TRNS,    KC_TRNS,                                            KC_TRNS,     KC_7,    KC_8, KC_9,  XXXXXXX,
-                                TO(BASE_LAYER), KC_LCTL, KC_LSFT,                          MO(FN_LAYER),  KC_SPC,  KC_BSPC
+        KC_TRNS,    KC_LEFT,    KC_UP,    KC_RIGHT,    KC_TRNS,                                             MO(FN_LAYER),    KC_4,    KC_5,    KC_6,    KC_TRNS,
+        KC_TRNS,    KC_TRNS,    KC_DOWN,    KC_TRNS,    KC_TRNS,                                            KC_LALT,     KC_7,    KC_8, KC_9,  XXXXXXX,
+                                KC_LGUI, KC_LCTL, KC_LSFT,                          XXXXXXX,  KC_SPC,  KC_BSPC
     ),
     [FN_LAYER] = LAYOUT(
         KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5, 
@@ -54,9 +49,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           TO(BASE_LAYER), KC_LCTL, KC_LSFT,                           KC_ENT,  KC_SPC,  KC_BSPC
     ),
     [GAME_LAYER] = LAYOUT(
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_1,    KC_2,   KC_3, KC_4,  KC_5, 
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_7,    KC_LEFT,  KC_UP, KC_RIGHT,  KC_8,
-        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_9,    KC_0,   KC_DOWN, XXXXXXX,  XXXXXXX,
-                                            KC_LCTL, KC_SPC, KC_LSFT,                 TO(BASE_LAYER),  XXXXXXX,  XXXXXXX
+        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_F,                               KC_1,    KC_2,   KC_3, KC_4,  KC_5, 
+        KC_LSFT,    KC_A,    KC_W,    KC_D,    KC_TRNS,                               KC_7,    KC_LEFT,  KC_UP, KC_RIGHT,  KC_8,
+        KC_TRNS,    KC_TRNS,    KC_S,    KC_TRNS,    KC_TRNS,                               KC_9,    KC_0,   KC_DOWN, XXXXXXX,  XXXXXXX,
+                                            KC_LCTL, KC_SPC, XXXXXXX,                 TO(BASE_LAYER),  XXXXXXX,  XXXXXXX
     ),
 };
